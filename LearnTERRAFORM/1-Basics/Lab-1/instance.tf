cdbb5900d27ec6f20f34ec2e864079a5681c3658
Nilesh Indore <indorenilesh@gmail.com>
2021-10-17 09:40:48 +0530

    update

--- LearnTERRAFORM/1-Basics/Lab-1/instance.tf
+++ LearnTERRAFORM/1-Basics/Lab-1/instance.tf
@@ -10,11 +10,11 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-resource "aws_instance" "test1" {
+resource "aws_instance" "web1" {
   ami           = "ami-083ebc5a49573896a"
   instance_type = "t2.micro"
 }
 
-resource "aws_eip" "test1" {
-instance = aws_instance.test1.id
+resource "aws_eip" "web1" {
+instance = aws_instance.web1.id
 }
